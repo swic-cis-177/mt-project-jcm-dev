f3e3e7594b2862e6b000b1276ab3330e7c7f5061
Josh Meyer <meyer.josh8@gmail.com>
2020-12-07 19:01:33 -0600

    Update data to include a next application date and format date to all double digit

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
 export default [
-  { name: "Josh Meyer", product: "Fertilizer", date: "1/1/01" },
-  { name: "John Johnson", product: "Weed Control", date: "1/2/01" },
-  { name: "Sara Silvers", product: "Fertilizer", date: "1/3/01" },
+  {
+    name: "Josh Meyer",
+    product: "Fertilizer",
+    date: "01/01/01",
+    next: "03/01/01",
+  },
+  {
+    name: "John Johnson",
+    product: "Weed Control",
+    date: "01/02/01",
+    next: "02/01/01",
+  },
+  {
+    name: "Sara Silvers",
+    product: "Fertilizer",
+    date: "01/03/01",
+    next: "03/01/01",
+  },
 ];
