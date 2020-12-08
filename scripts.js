52d299ef6280ccdd3b8ec429e1c01bc4760a11e7
Josh Meyer <meyer.josh8@gmail.com>
2020-12-07 18:19:48 -0600

    Import data from data.js and log to the console to test

--- scripts.js
+++ scripts.js
@@ -1,34 +1,38 @@
+import data from "./data.js";
+
+console.log(data);
+
 // const date = document.querySelector("#date");
 
 // date.addEventListener("focus", () => {
 //   console.log("Input entered");
 // });
 
-const objTest = {
-  date: document.querySelector("#date"),
-  productType: document.querySelector("#type"),
-  chemical: document.querySelector("#chem"),
-  frequency: document.querySelector("#frequency"),
-};
+// const objTest = {
+//   date: document.querySelector("#date"),
+//   productType: document.querySelector("#type"),
+//   chemical: document.querySelector("#chem"),
+//   frequency: document.querySelector("#frequency"),
+// };
 
-const arrayTest = new Array(0);
+// const arrayTest = new Array(0);
 
-for (const property in objTest) {
-  objTest[property].addEventListener("focus", () => {
-    console.log("Input entered");
-  });
-}
-//testing
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
+// for (const property in objTest) {
+//   objTest[property].addEventListener("focus", () => {
+//     console.log("Input entered");
+//   });
+// }
+// //testing
+// document.querySelector("form").addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  for (const property in objTest) {
-    console.log(objTest[property]);
-  }
+//   for (const property in objTest) {
+//     console.log(objTest[property]);
+//   }
 
-  arrayTest.push(objTest);
+//   arrayTest.push(objTest);
 
-  for (let i = 0; i < arrayTest.length; i++) {
-    console.log(arrayTest[i]);
-  }
-});
+//   for (let i = 0; i < arrayTest.length; i++) {
+//     console.log(arrayTest[i]);
+//   }
+// });
