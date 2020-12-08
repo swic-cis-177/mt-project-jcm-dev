a9a07cac0c376e93a986ec894d169e46fd03be6c
Josh Meyer <meyer.josh8@gmail.com>
2020-12-08 01:42:12 -0600

    Misc edit

--- scripts.js
+++ scripts.js
@@ -7,44 +7,6 @@ createCustomerTable(data);
 document.querySelector("form").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  data.push(
-    // Send 'name' and 'score' from the form
-    generateCustomer(event.target.elements)
-  );
+  data.push(generateCustomer(event.target.elements));
   createCustomerTable(data);
 });
-
-// const date = document.querySelector("#date");
-
-// date.addEventListener("focus", () => {
-//   console.log("Input entered");
-// });
-
-// const objTest = {
-//   date: document.querySelector("#date"),
-//   productType: document.querySelector("#type"),
-//   chemical: document.querySelector("#chem"),
-//   frequency: document.querySelector("#frequency"),
-// };
-
-// const arrayTest = new Array(0);
-
-// for (const property in objTest) {
-//   objTest[property].addEventListener("focus", () => {
-//     console.log("Input entered");
-//   });
-// }
-// //testing
-// document.querySelector("form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   for (const property in objTest) {
-//     console.log(objTest[property]);
-//   }
-
-//   arrayTest.push(objTest);
-
-//   for (let i = 0; i < arrayTest.length; i++) {
-//     console.log(arrayTest[i]);
-//   }
-// });
