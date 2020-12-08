7098f69590dd785c6c5ddb68b5b43a62b286315f
Josh Meyer <meyer.josh8@gmail.com>
2020-12-07 18:11:33 -0600

    Create data file of an array of customer objects

---
+++ data.js
@@ -0,0 +1,5 @@
+export default [
+  { name: "Josh Meyer", Product: "Fertilizer", Date: "1/1/01" },
+  { name: "John Johnson", Product: "Weed Control", Date: "1/2/01" },
+  { name: "Sara Silvers", Product: "Fertilizer", Date: "1/3/01" },
+];
